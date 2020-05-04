1e83309f36aff8eedc55f3becf854ef3f6d774b4
catdog50rus <49200747+catdog50rus@users.noreply.github.com>
2020-05-04 18:17:15 +0300

    AddModels

---
+++ TrainConsistEdition.BL.Models/Trains/TrainVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainConsistEdition.BL.Models.Trains
+{
+    public class TrainVehicle
+    {
+        public string Module { get; set; }
+        public string ModuleConfig { get; set; }
+        public int Count { get; set; }
+        public double PayloadCoeff { get; set; }
+
+
+    }
+}
